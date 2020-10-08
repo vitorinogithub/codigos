67397cee8e13f42033ab9231da2b91005a1eba23
Joao Neto <joao.b.neto@capgemini.com>
2020-10-07 22:58:11 -0300

    Outras Atualizações
    Outras Atualizações

--- WebApiCalculaJuros/WebApiCalculaJuros/obj/Debug/netcoreapp3.1/WebApi2.AssemblyInfo.cs
+++ WebApiCalculaJuros/WebApiCalculaJuros/obj/Debug/netcoreapp3.1/WebApi2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057a42b9f40a2e6308a5d64c35e975bd86757ec0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
